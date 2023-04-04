f92fb44f046245c57e7c93c1bd1881db898adaac
kethnal <kaseyvaldez@hotmail.com>
2023-04-04 06:44:50 -0700

    Style for hero and about for mobile

--- src/components/Homepage/About.jsx
+++ src/components/Homepage/About.jsx
@@ -1,7 +1,26 @@
 function About() {
   return (
     <div className="about">
-      About
+      <img
+        className="about_image_first"
+        src="image-about-dark.jpg"
+        alt="dark chairs in a dark room with round wooden table"
+      />
+
+      <div className="about_content">
+        <h2 className="about_content_header">
+          ABOUT OUR FURNITURE
+        </h2>
+        <p className="about_content_description">
+          Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.
+        </p>
+      </div>
+
+      <img
+        className="about_image_second"
+        src="image-about-light.jpg"
+        alt="white chair in a all white room"
+      />
     </div>
   );
 }
--- src/components/Homepage/Hero.jsx
+++ src/components/Homepage/Hero.jsx
@@ -1,17 +1,26 @@
 function Hero() {
   return (
     <div className="hero">
-      <div className="hero_arrow-buttons">
-        <div className="hero_arrow_buttons_left-container">
-          <img src="icon-angle-left.svg" alt="see previous image" />
-        </div>
-        <div className="hero_arrow_buttons_right-container">
-          <img src="icon-angle-right.svg" alt="see next image" />
-        </div>
-      </div>
 
       <div className="hero_image-container">
-        <img src="mobile-image-hero-1.jpg" alt="chair assortment" />
+        <img
+          src="mobile-image-hero-1.jpg"
+          alt="chair assortment"
+          className="hero_image-container_image"
+        />
+
+        <div className="hero_image-container_arrow-buttons">
+          <img
+            src="icon-angle-left.svg"
+            alt="see previous image"
+            className="hero_image-container_arrow-buttons_left"
+            />
+          <img
+            src="icon-angle-right.svg"
+            alt="see next image"
+            className="hero_image-container_arrow-buttons_right"
+          />
+        </div>
       </div>
 
       <div className="hero_content">
@@ -23,7 +32,10 @@ function Hero() {
           We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.
         </p>
 
-        <button className="hero_content_button">SHOP NOW</button>
+        <button className="hero_content_button">
+          SHOP NOW
+          <img src="icon-arrow.svg" alt="right arrow" />
+        </button>
       </div>
     </div>
   );

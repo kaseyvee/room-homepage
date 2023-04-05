9c64914f7a4d04a04c12dc1abae0e1bca6451758
kethnal <kaseyvaldez@hotmail.com>
2023-04-04 22:49:00 -0700

    Put hero info into object and implement interactive arrow functionality

--- src/components/Homepage/Hero.jsx
+++ src/components/Homepage/Hero.jsx
@@ -1,10 +1,53 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import heroItems from "../../helpers/heroItems";
+
 function Hero() {
+  const [heroItem, setHeroItem] = useState(0);
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    handleCheckIfDesktop();
+  }, [])
+
+  // setInterval(() => {
+  //   return handleRotateItems("next");
+  // }, 5000);
+
+  function handleRotateItems(action) {
+    if (action === "next") {
+      if (heroItem === 2) {
+        return setHeroItem(0);
+      }
+
+      return setHeroItem(heroItem + 1);
+    }
+
+    if (action === "prev") {
+      if (heroItem === 0) {
+        return setHeroItem(2);
+      }
+    
+      return setHeroItem(heroItem - 1);
+    }
+  }
+
+  function handleCheckIfDesktop() {
+    if (window.innerWidth < 595) {
+      return setIsDesktop(false);
+    }
+
+    return setIsDesktop(true);
+  }
+
+  addEventListener("resize", handleCheckIfDesktop);
+
   return (
     <div className="hero">
 
       <div className="hero_image-container">
         <img
-          src="mobile-image-hero-1.jpg"
+          src={`${isDesktop ? "desktop" : "mobile"}-image-hero-${heroItem + 1}.jpg`}
           alt="chair assortment"
           className="hero_image-container_image"
         />
@@ -14,22 +57,24 @@ function Hero() {
             src="icon-angle-left.svg"
             alt="see previous image"
             className="hero_image-container_arrow-buttons_left"
+            onClick={() => handleRotateItems("prev")}
             />
           <img
             src="icon-angle-right.svg"
             alt="see next image"
             className="hero_image-container_arrow-buttons_right"
+            onClick={() => handleRotateItems("next")}
           />
         </div>
       </div>
 
       <div className="hero_content">
         <h1 className="hero_content_header">
-          Discover innovative ways to decorate
+          {heroItems[heroItem].header}
         </h1>
 
         <p className="hero_content_description">
-          We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.
+          {heroItems[heroItem].description}
         </p>
 
         <button className="hero_content_button">
